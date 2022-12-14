84295a77898e60912ad3f4e0462fee8cf63e020b
Matt Burke <39552849+mattdburke@users.noreply.github.com>
2022-12-14 13:05:14 +0000

    Create CAPM.r

---
+++ CAPM.r
@@ -0,0 +1,28 @@
+setwd("C:/Users/mb0221/Downloads")
+df1 <- read.csv("capm.csv")
+df2 <- data.frame(matrix(ncol = 3, nrow = 8))
+colnames(df2) <- c("Firm", "AvRet", "Beta")
+df2$Firm <- colnames(df1)
+calculate_beta <- function(stock_ticker)
+	{
+	stock_ret <- df1[[stock_ticker]]
+	market <- df1$MKT
+	fit <- lm(stock_ret ~ market)
+	return (fit$coef[2])
+	}
+betas <- list()
+for (i in df2$Firm){
+	betas[[length(betas) + 1]] <-  calculate_beta(as.character(i))}
+df2$Beta <- betas
+calculate_AvRet <- function(stock_ticker)
+	{
+	stock_ret <- df1[[stock_ticker]]
+	return (mean(stock_ret))
+	}
+ret <- list()
+for (i in df2$Firm){
+	ret[[length(ret) + 1]] <-  calculate_AvRet(as.character(i))}
+df2$AvRet <- ret
+CAPM <- lm(as.numeric(df2$AvRet) ~ as.numeric(df2$Beta))
+plot(df2$Beta, df2$AvRet)
+abline(CAPM)
